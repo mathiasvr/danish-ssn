e9c543dbf88439ac13ce42026e3adffdaffd4e33
Paweł Bród <pawel.brod@gmail.com>
2019-02-01 15:51:42 +0100

    fix styling

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var pad = require('pad-left')
 
-
-var multipliers = [4, 3, 2, 7, 6, 5, 4, 3, 2, 1];
+var multipliers = [4, 3, 2, 7, 6, 5, 4, 3, 2, 1]
 
 function sumProduct (a, b) {
   return a.reduce(function (sum, digit, i) { return sum + digit * b[i] }, 0)
@@ -42,22 +41,22 @@ function validate (cpr) {
 function getDate (cpr) {
   var digit7 = parseInt(cpr[6], 10)
 
-  var dateSegments = cpr.substring(0, 6).match(/.{1,2}/g);
-  var date = new Date(Date.UTC(dateSegments[2], dateSegments[1]-1, dateSegments[0], 0,0,0,0));
+  var dateSegments = cpr.substring(0, 6).match(/.{1,2}/g)
+  var date = new Date(Date.UTC(dateSegments[2], dateSegments[1] - 1, dateSegments[0], 0, 0, 0, 0))
 
-  var year = date.getFullYear();
-  var month = date.getMonth();
-  var day = date.getDate();
+  var year = date.getFullYear()
+  var month = date.getMonth()
+  var day = date.getDate()
 
   // century correction
   if (digit7 === 4 || digit7 === 9) {
     // 1900 or 2000
     if (year <= 1936) {
-      date =  new Date(Date.UTC(year + 100, month, day, 0, 0, 0, 0))
+      date = new Date(Date.UTC(year + 100, month, day, 0, 0, 0, 0))
     }
   } else if (digit7 > 4) {
     // 1800 or 2000
-    date =  new Date(Date.UTC(year > 1957 ? year-100 : year+100, month, day, 0, 0,0, 0));
+    date = new Date(Date.UTC(year > 1957 ? year - 100 : year + 100, month, day, 0, 0, 0, 0))
   }
 
   return date
@@ -80,11 +79,11 @@ function getInfo (cpr) {
 function validForDate (date) {
   var year = date.getFullYear()
 
-  var formattedDate = ('0' + date.getDate()).slice(-2);
-  var formattedMonth = ('0' + (date.getMonth() + 1)).slice(-2);
-  var formattedYear = date.getFullYear().toString().substr(2,2);
+  var formattedDate = ('0' + date.getDate()).slice(-2)
+  var formattedMonth = ('0' + (date.getMonth() + 1)).slice(-2)
+  var formattedYear = date.getFullYear().toString().substr(2, 2)
 
-  var dateString = formattedDate + formattedMonth + formattedYear;
+  var dateString = formattedDate + formattedMonth + formattedYear
 
   var validNumbers = []
 
